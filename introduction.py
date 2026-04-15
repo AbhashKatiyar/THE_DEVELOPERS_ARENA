c58bd934172242f61eb854181fa4fabc60d2e3f2
Abhash Katiyar <abhash.singhkatiyar@gmail.com>
2026-04-15 19:54:10 +0530

    First Commit in Introduction

---
+++ introduction.py
@@ -0,0 +1,20 @@
+# Welcome Program
+# This program welcomes the user and asks for some basic information about them. It then displays a friendly message based on the input.
+
+print("🌟 Welcome to the Friendly Chat Program! 🌟\n")
+
+# Taking user inputs
+name = input("What's your name? \n")
+age = input("How old are you? \n")
+city = input("Which city do you live in? \n")
+profession = input("What is your profession? \n")
+workplace = input("Where do you work? \n")
+hobby = input("What is your favorite hobby? \n")
+
+# Displaying a friendly message
+print("\n😊 Nice to meet you,", name + "!")
+print("You are", age, "years old and live in", city + ".")
+print("It's great that you work as a", profession + " at", workplace + ".")
+print("It's awesome that you enjoy", hobby + "!🎯")
+
+print("\n🎉 Thank you for sharing! Have a great day! 🎉")
